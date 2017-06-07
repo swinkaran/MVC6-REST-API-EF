31e96df35f7b41ed4cfeb3b7aaf6a158e7e3c618
Swinkaran <swinkaran@hotmail.com>
2017-06-07 23:48:04 +1000

    Including the API project
    - Including the API project
    - Add Stock entities service

---
+++ YBoard-API/YBoardAPI.Service/StockService.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YBoardAPI.Model;
+using YBoardAPI.Data;
+using YBoardAPI.Data.Repository;
+using YBoardAPI.Data.Infrastructure;
+
+namespace YBoardAPI.Service
+{
+    public class StockService : IStockService
+    {
+        public IStockRepository ITradeDayRepository;
+        private readonly IUnitOfWork unitOfWork;
+
+        public StockService(IStockRepository tradeDay, IUnitOfWork unitOfWork)
+        {
+            ITradeDayRepository = tradeDay;
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IList<TradeDay> GetAllStocks()
+        {
+            return ITradeDayRepository.GetAll().ToList();
+        }
+
+        public IEnumerable<TradeDay> GetTradeDays()
+        {
+            return ITradeDayRepository.GetAll();
+        }
+
+        public IEnumerable<TradeDay> GetTradeDays(long id)
+        {
+            return ITradeDayRepository.GetAll().Where(t => t.CompanyId == id).ToList();
+        }
+
+        public void SaveArticle()
+        {
+            unitOfWork.Commit();
+        }
+    }
+
+    public interface IStockService
+    {
+        IEnumerable<Model.TradeDay> GetTradeDays();
+        IEnumerable<Model.TradeDay> GetTradeDays(long id);
+        void SaveArticle();
+    }
+}
